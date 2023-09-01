a3e860bb4b7af34b0f7ce9e7614709b4756d049e
Milky <milky-maggie@mai.ru>
2023-09-01 21:17:46 +0300

    14 sprint

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const { validateLogin, validateCreateUser } = require('./middlewares/validator');
 const {
   NOT_FOUND,
 } = require('./errors/errors');
@@ -21,8 +22,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', validateLogin, login);
+app.post('/signup', validateCreateUser, createUser);
 
 app.use('/users', auth, userRouter);
 app.use('/cards', auth, cardRouter);
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,12 @@ const router = require('express').Router();
 const {
   getUsers, getUser, getUserById, updateProfile, updateAvatar,
 } = require('../controllers/users');
+const { validateUserId, validateUpdateProfile, validateUpdateAvatar } = require('../middlewares/validator');
 
 router.get('/', getUsers);
-router.get('/:userId', getUserById);
-router.patch('/me', updateProfile);
-router.patch('/me/avatar', updateAvatar);
+router.get('/:userId', validateUserId, getUserById);
+router.patch('/me', validateUpdateProfile, updateProfile);
+router.patch('/me/avatar', validateUpdateAvatar, updateAvatar);
 router.get('/me', getUser);
 
 module.exports = router;
